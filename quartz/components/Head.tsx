9891d814da2cd4c127748f8cab893834db17fabc
Chandan Nayak <chandan.phoenix@gmail.com>
2026-01-20 20:32:21 +0530

    Add og:image width and height for social media preview

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -66,6 +66,8 @@ export default (() => {
           <>
             <meta property="og:image" content={ogImageDefaultPath} />
             <meta property="og:image:url" content={ogImageDefaultPath} />
+            <meta property="og:image:width" content="1024" />
+            <meta property="og:image:height" content="1024" />
             <meta name="twitter:image" content={ogImageDefaultPath} />
             <meta
               property="og:image:type"
